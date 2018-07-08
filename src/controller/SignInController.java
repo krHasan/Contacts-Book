1ba4e41acaad8732eb6cbb8c802b5ee05f697016
krHasan <hasanbappi@gmail.com>
2018-07-08 19:48:52 +0600

    SignIn window UI complete

---
+++ src/controller/SignInController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class SignInController {
+	@FXML
+	private GridPane gridPane;
+	
+	@FXML
+	private TextField username;
+	
+	@FXML
+	private PasswordField password;
+	
+	@FXML
+	private Button signIn;
+	
+	@FXML
+	private Label forgotPassword;
+	
+	@FXML
+	private Label newUser;
+	
+	@FXML
+	private Label warning;
+	
+	
+	
+	
+}
---
+++ src/view/SignIn.java
@@ -0,0 +1,26 @@
+package view;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class SignIn extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("SignIn.fxml"));
+			Scene scene = new Scene(root,800,600);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
